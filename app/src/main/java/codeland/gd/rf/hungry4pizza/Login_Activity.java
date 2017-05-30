8f5eeed79c3c9a69f30c1397347fbcc21f55233e
aledebarba <ale@alemacedo.com>
2017-05-29 22:46:29 -0300

    finaldaaula

---
+++ app/src/main/java/codeland/gd/rf/hungry4pizza/Login_Activity.java
@@ -0,0 +1,18 @@
+package codeland.gd.rf.hungry4pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+    }
+
+    public void logar(View view) {
+        
+    }
+}
---
+++ app/src/main/java/codeland/gd/rf/hungry4pizza/pedido.java
@@ -0,0 +1,13 @@
+package codeland.gd.rf.hungry4pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido);
+    }
+}
